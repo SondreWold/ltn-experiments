2c53dfbf76689a36ec22c8de055537d1d25b9bb5
Sondre Wold <sondrewold1@gmail.com>
2023-04-28 16:10:00 +0200

    Train script blueprint

---
+++ train.py
@@ -0,0 +1,121 @@
+import argparse
+import random
+import torch.nn as nn
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModel, AutoModelForMultipleChoice
+from transformers import TrainingArguments, Trainer
+from torch.utils.data import DataLoader, Dataset
+from torch.optim import AdamW
+from torch.nn import CrossEntropyLoss
+from tqdm import tqdm
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from datasets import Dataset as Ds
+import numpy as np
+import ltn
+import logging 
+from model import SequenceModel
+from dataset import dataset_creator
+device = 'cuda:0' if torch.cuda.is_available() else 'mps'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="ArgumentParser for GLUE scripts")
+    parser.add_argument("--batch_size", type=int, default=16, help="The batch size to use during training.")
+    parser.add_argument("--weight_decay", type=float, default=0.01, help="The weight decay to use during training.") 
+    parser.add_argument("--model_name", type=str, default="bert-base-uncased", help="The pretrained model to use")
+    parser.add_argument("--task", type=str, default="rte", help="The task to train on")
+    parser.add_argument("--epochs", type=int, default=3, help="The number of epochs.")
+    parser.add_argument("--debug", action='store_true', help="Trigger debug mode")
+    parser.add_argument("--freeze", action='store_true', help="Only fine tune classification head")
+    parser.add_argument("--test", action='store_true', help="Trigger test eval")
+    parser.add_argument("--lr", type=float, default=3e-5, help="The learning rate).")
+    parser.add_argument("--seed", type=int, default=42, help="The rng seed")
+    parser.add_argument("--gradient_clip", action='store_true', help="The gradient clip")
+    parser.add_argument("--beta", type=float, default=1, help="The adam momentum")
+    parser.add_argument("--patience", type=int, default=2, help="The patience value")
+    parser.add_argument("--dropout", type=float, default=0.2, help="The dropout value")
+
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    print("====================================================================================================")
+    logging.info(f"Initialised training on task: {args.task.upper()}, debug={args.debug}, device={device}")
+    print("====================================================================================================")
+    print("\n")
+
+    config = {
+            "task": args.task,
+            "learning_rate": args.lr,
+            "weight_decay": args.weight_decay,
+            "epochs": args.epochs,
+            "batch_size": args.batch_size,
+            "seed": args.seed,
+            "model": args.model_name,
+            "frozen": args.freeze,
+            "test": args.test,
+            "dropout": args.dropout,
+        }
+
+    train_dataset = dataset_creator(args.task)(tokenizer_path_or_name=args.model_name, root_data_path="./data", split='train', max_length=128)
+    val_dataset = dataset_creator(args.task)(tokenizer_path_or_name=args.model_name, root_data_path="./data", split='dev', max_length=128)
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=1, drop_last=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=1, drop_last=True)
+
+    model = SequenceModel(args.model_name, 2, args.dropout).to(device)
+    optimizer = AdamW(model.parameters(), lr=args.lr)
+    criterion = CrossEntropyLoss()
+
+    for epoch in range(10):
+        logging.info(f"Started training at epoch {epoch}")
+        model.train()
+        train_loss = 0.0
+        for i, (input_ids, attention_masks, y) in enumerate(tqdm(train_loader)):
+            input_ids, attention_masks, y = input_ids.to(device), attention_masks.to(device), y.to(device)
+            out = model(input_ids, attention_masks)
+            loss = criterion(out, y.squeeze(-1))
+            train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        train_loss = train_loss / len(train_loader)
+
+        model.eval()
+        with torch.no_grad():
+            val_loss = 0.0
+            mean_accuracy = 0.0
+            for i, (input_ids, attention_masks, y) in enumerate(tqdm(val_loader)):
+                input_ids, attention_masks, y = input_ids.to(device), attention_masks.to(device), y.to(device)
+                out = model(input_ids, attention_masks)
+                loss = criterion(out, y.squeeze(-1))
+                train_loss += loss.item()
+                predictions = np.argmax(out.cpu(), axis=1)
+                acc = accuracy_score(y.cpu(), predictions)
+                mean_accuracy += acc
+
+            mean_accuracy /= len(val_loader)
+            val_loss /= len(val_loader)
+
+            logging.info(" epoch %d | train loss %.4f | Val loss %.3f | Val Acc %.3f"
+                %(epoch, train_loss, val_loss, mean_accuracy))
+
+if __name__ == "__main__":
+    args = parse_args()
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    main(args)
