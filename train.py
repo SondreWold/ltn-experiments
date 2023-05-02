69ffe302145e0eb2aefcd2679fcaa96f7d43a8a6
Sondre Wold <sondrewold1@gmail.com>
2023-05-02 12:37:37 +0200

    Add schedulers and weight decay

--- train.py
+++ train.py
@@ -14,7 +14,8 @@ from sklearn.metrics import accuracy_score
 from datasets import Dataset as Ds
 import numpy as np
 import ltn
-import logging 
+import logging
+from torch.optim import lr_scheduler
 from model import SequenceModel
 from dataset import dataset_creator
 import os
@@ -49,6 +50,8 @@ def parse_args():
     parser.add_argument("--weight_decay", type=float, default=0.01, help="The weight decay to use during training.") 
     parser.add_argument("--model_name", type=str, default="bert-base-uncased", help="The pretrained model to use")
     parser.add_argument("--task", type=str, default="rte", help="The task to train on")
+    parser.add_argument("--optimizer", type=str, default="adamw", help="The optimizer function to train with")
+    parser.add_argument("--scheduler", type=str, default="cosine", help="The scheduler to train with")
     parser.add_argument("--epochs", type=int, default=3, help="The number of epochs.")
     parser.add_argument("--debug", action='store_true', help="Trigger debug mode")
     parser.add_argument("--freeze", action='store_true', help="Only fine tune classification head")
@@ -75,6 +78,8 @@ def main(args):
             "weight_decay": args.weight_decay,
             "epochs": args.epochs,
             "batch_size": args.batch_size,
+            "scheduler": args.scheduler,
+            "optimizer": args.optimizer,
             "seed": args.seed,
             "model": args.model_name,
             "freeze": args.freeze,
@@ -96,7 +101,46 @@ def main(args):
     logging.info(train_dataset.get_decoded_example(13))
 
     model = SequenceModel(args.model_name, number_of_labels[args.task.upper()], args.dropout).to(device)
-    optimizer = AdamW(model.parameters(), lr=args.lr)
+
+    params = list(model.named_parameters())
+    no_decay = ['bias', 'layer_norm', 'embedding']
+    decay_params = [(n, p) for n, p in params if not any(nd in n for nd in no_decay)]
+    no_decay_params = [(n, p) for n, p in params if any(nd in n for nd in no_decay)]
+    optimizer_grouped_parameters = [
+        {'params': [p for _, p in decay_params], 'weight_decay': args.weight_decay},
+        {'params': [p for _, p in no_decay_params], 'weight_decay': 0.0}
+    ]
+
+    if args.optimizer == "adam" or args.optimizer == "adamw":
+        optimizer = torch.optim.AdamW(
+            optimizer_grouped_parameters,
+            lr=args.lr,
+            betas=(0.9, 0.98),
+            eps=1e-6,
+        )
+    else:
+        pass
+
+    if args.scheduler == "cosine":
+        def cosine_schedule_with_warmup(optimizer, num_warmup_steps: int, num_training_steps: int, min_factor: float):
+            def lr_lambda(current_step):
+                if current_step < num_warmup_steps:
+                    return float(current_step) / float(max(1, num_warmup_steps))
+                progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
+                lr = max(min_factor, min_factor + (1 - min_factor) * 0.5 * (1.0 + math.cos(math.pi * progress)))
+
+                return lr
+
+            return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
+
+        scheduler = cosine_schedule_with_warmup(optimizer, int(args.device_max_steps * args.warmup_proportion), args.device_max_steps, 0.1)
+
+    elif args.scheduler == "linear":
+        scheduler = lr_scheduler.ChainedScheduler([
+            lr_scheduler.LinearLR(optimizer, start_factor=1e-9, end_factor=1.0, total_iters=int(args.device_max_steps * args.warmup_proportion)),
+            lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=1e-9, total_iters=args.device_max_steps)
+        ])
+
     criterion = CrossEntropyLoss()
 
     if args.freeze:
@@ -115,6 +159,7 @@ def main(args):
             train_loss += loss.item()
             loss.backward()
             optimizer.step()
+            scheduler.step()
 
         model.eval()
         with torch.no_grad():
